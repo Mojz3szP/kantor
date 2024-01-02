fb6fe33c21692a2459c3506ffa334d4b2b079b07
Mojz3szP <paradowski1200@gmail.com>
2024-01-02 22:52:01 +0100

    use prettier and change readme

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # Wymiana walut
+
 [Kantor - klinij tutaj](https://mojz3szp.github.io/kantor/kantor.html)
 
 ## Kursy
+
+Zapraszam do naszego kantoru, gdzie mamy najlepsze kursy na rynku.
+
 1. EUR - 4,48
 2. GBP - 5,22
 3. USD - 4,15
 4. CHF - 4,62
 
-![ADD](https://github.com/Mojz3szP/kantor/blob/main/81894_matematyka_dodawanie_liczby.jpg)
+![ADD](./image/Zrzut%20ekranu.png)
--- js/kantor.js
+++ js/kantor.js
@@ -1,43 +1,41 @@
-
-
 const calculateResult = (amount, currency) => {
-    const GBP = 5.22;
-    const EUR = 4.48;
-    const USD = 4.15;
-    const CHF = 4.62;    
-switch (currency) {
+  const GBP = 5.22;
+  const EUR = 4.48;
+  const USD = 4.15;
+  const CHF = 4.62;
+  switch (currency) {
     case "EUR":
-        return amount / EUR;
-    
-     case "USD":
-        return amount / USD;
-        
+      return amount / EUR;
+
+    case "USD":
+      return amount / USD;
+
     case "CHF":
-        return amount / CHF;
-        
+      return amount / CHF;
+
     case "GBP":
-        return amount / GBP;
-        
-    }}
+      return amount / GBP;
+  }
+};
 
 const onFormSubmit = (event) => {
-        event.preventDefault(); 
-        const currencyElement = document.querySelector(".js-currency");
-        const amountElement = document.querySelector(".js-amount");
-        const resultElement = document.querySelector(".js-result");
-        const amount = +amountElement.value;
-        const currency = currencyElement.value;
-        let result = calculateResult(amount, currency);
-    
-       
-    
-        resultElement.innerHTML= `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}<strong>`;
-        };
+  event.preventDefault();
+  const currencyElement = document.querySelector(".js-currency");
+  const amountElement = document.querySelector(".js-amount");
+  const resultElement = document.querySelector(".js-result");
+  const amount = +amountElement.value;
+  const currency = currencyElement.value;
+  let result = calculateResult(amount, currency);
+
+  resultElement.innerHTML = `${amount.toFixed(
+    2
+  )} PLN = <strong>${result.toFixed(2)} ${currency}<strong>`;
+};
 
 const init = () => {
- const formElement = document.querySelector(".js-form");
+  const formElement = document.querySelector(".js-form");
 
-formElement.addEventListener("submit", onFormSubmit)
-    };
+  formElement.addEventListener("submit", onFormSubmit);
+};
 
- init();
+init();
